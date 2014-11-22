22cdf55e64d0c31ba078c78641f0ba99c952f1ae
Jungtaek Kim <jungtaek.kim@jt-inc.net>
2014-11-22 11:53:42 -0500

    add

--- JT Inc./titanic/trial02/nb.py
+++ JT Inc./titanic/trial02/nb.py
@@ -39,7 +39,7 @@ for row in csv_file_object:
 features = []
 data = np.array(data)
 
-features = data[:, [5, 6]].astype(np.float)
+features = data[:, [4, 6]].astype(np.float)
 output = data[:, 1]
 
 clf = GaussianNB()
@@ -79,7 +79,7 @@ for row in test_file_object:
 	else:
 		row[5] = float(row[5])
 
-	prediction_file_object.writerow([row[0], clf.predict([[row[4], row[5]]])[0]])
+	prediction_file_object.writerow([row[0], clf.predict([[row[3], row[5]]])[0]])
 
 test_file.close()
 prediction_file.close()
