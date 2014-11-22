f54ad69550f73aeeb94b7f15f2a0d8f163ff7fa9
Jungtaek Kim <jungtaek.kim@jt-inc.net>
2014-11-22 03:35:23 -0500

    update

--- JT Inc./titanic/trial02/nb.py
+++ JT Inc./titanic/trial02/nb.py
@@ -5,36 +5,32 @@ from sklearn.naive_bayes import GaussianNB
 csv_file_object = csv.reader(open('train.csv', 'rb'))
 header = csv_file_object.next()
 data=[]
+count_row = 0
 
 for row in csv_file_object:
+	if row[4] == "female":
+		row[4] = 1.0
+	else:
+		row[4] = 0.0
+
+	if row[9] == "":
+		row[9] = 0.0
+	else:
+		row[9] = float(row[9])
+
+	if row[5] == "":
+		row[5] = 0.0
+	else:
+		row[5] = float(row[5])
+
 	data.append(row)
-
-data = np.array(data)
-
-women_only_stats = data[:, 4] == "female"
-men_only_stats = data[:, 4] == "male"
-
-women_onboard = women_only_stats.astype(np.float)
-men_onboard = men_only_stats.astype(np.float)
-
-id_stats = data[:, 0].astype(np.float)
-fare_stats = data[:, 9].astype(np.float)
-
-id_stats = id_stats.tolist()
-fare_stats = fare_stats.tolist()
-women_only_stats = women_onboard.tolist()
+	count_row += 1
 
 features = []
+data = np.array(data)
 
-number_passengers = np.size(data[:, 1].astype(np.float))
-
-for i in range(0, number_passengers):
-	features.append([fare_stats[i], women_only_stats[i]])
-
+features = data[:, [4, 9]].astype(np.float)
 output = data[:, 1]
-
-features = np.array(features)
-output = np.array(output)
 
 clf = GaussianNB()
 clf.fit(features, output)
@@ -49,12 +45,16 @@ prediction_file_object.writerow(["PassengerId", "Survived"])
 
 for row in test_file_object:
 	if row[3] == "female":
-		prediction_file_object.writerow([row[0], clf.predict([[1.0, float(row[8])]])[0]])
+		row[3] = 1.0
 	else:
-		if row[8] != "":
-			prediction_file_object.writerow([row[0], clf.predict([[0.0, float(row[8])]])[0]])
-		else:
-			prediction_file_object.writerow([row[0], clf.predict([[0.0, 0.0]])[0]])
+		row[3] = 0.0
+
+	if row[8] == "":
+		row[8] = 0.0
+	else:
+		row[8] = float(row[8])
+
+	prediction_file_object.writerow([row[0], clf.predict([[row[3], row[8]]])[0]])
 
 test_file.close()
 prediction_file.close()
